c374920a1f8ffefbe734ba0b50a3a7c0e3eebe1c
OscarOchoaRamon <140092213+OscarOchoaRamon@users.noreply.github.com>
2025-12-23 01:57:54 -0500

    Refactor font registration and page setup

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def navigate_to(page):
 # --- VIEWS ---
 
 def landing_page():
-    st.title("💧 Calidad ambiental")
+    st.title("🌍 Calidad ambiental")
     st.markdown("### Seleccione el módulo que desea consultar:")
     
     col1, col2 = st.columns(2)
@@ -250,5 +250,6 @@ elif st.session_state['page'] == 'surface':
     water_quality_module(module_type="surface")
 elif st.session_state['page'] == 'effluents':
     water_quality_module(module_type="effluents")
+
 
 

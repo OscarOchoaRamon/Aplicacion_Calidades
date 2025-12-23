97e47ec1451943c3192bf20139b8f3359b492f60
OscarOchoaRamon <140092213+OscarOchoaRamon@users.noreply.github.com>
2025-12-23 14:17:58 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -272,8 +272,8 @@ def create_chart(df, parameter, selected_columns=None, date_angle=-90, legend_po
     legend_layout = dict(
         font=dict(family="Bookman Old Style, serif", size=7, color="black"),
         title=dict(text=""),
-        tracegroupgap=0,
-        itemwidth=20
+        tracegroupgap=5,
+        itemwidth=30
     )
     
     # Margin adjustments based on legend
@@ -385,6 +385,7 @@ def create_chart(df, parameter, selected_columns=None, date_angle=-90, legend_po
     fig.update_layout(font=dict(family="Bookman Old Style, serif", size=9, color="black"))
     
     return fig
+
 
 
 
